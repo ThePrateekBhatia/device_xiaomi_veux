c8a0a0c5ae162bba61ff93d683a55cb5127cd711
Nippon <126399437+nnippon99@users.noreply.github.com>
2023-04-22 13:55:22 +0200

    veux: device.mk: drop deprecated packages

--- device.mk
+++ device.mk
@@ -48,7 +48,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audioadsprpcd \
-    audio.primary.holi \
     audio.r_submix.default \
     audio.usb.default
 
@@ -259,8 +258,6 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxG711Enc \
     libOmxQcelp13Enc \
-    libOmxVdec \
-    libOmxVenc \
     libstagefrighthw \
     libstagefright_omx \
     libstagefright_foundation \
