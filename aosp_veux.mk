27f737576a7a7902aef611e9a31e2df72f4cd28d
Nippon <adamayyad1999@hotmail.com>
2023-08-15 10:36:12 +0300

    veux: Add support for FaceUnlock
    Signed-off-by: Nippon <adamayyad1999@hotmail.com>

--- aosp_veux.mk
+++ aosp_veux.mk
@@ -8,6 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
