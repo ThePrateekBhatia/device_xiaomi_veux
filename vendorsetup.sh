65ca1d938e4c5a55aa3162f52b87f54168490b7d
Prateek Bhatia <bhatiaprateek111@gmail.com>
2023-12-13 15:48:50 +0530

    veux: introduces vendorsetup.sh
    Signed-off-by: Prateek Bhatia <bhatiaprateek111@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+rm -rf hardware/qcom-caf/sm8350/display
+git clone https://github.com/Evolution-X/hardware_qcom-caf_sm8350_display hardware/qcom-caf/sm8350/display
+git clone https://gitlab.com/ThePrateekBhatia/vendor-xiaomi-veux.git vendor/xiaomi/veux
+git clone https://github.com/ThePrateekBhatia/kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375
+echo 'Completed, now you can proceed to lunch..!'
