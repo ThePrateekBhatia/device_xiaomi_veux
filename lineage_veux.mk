73089558631a57b27c77934fb1085cfcdc4fa38c
Prateek Bhatia <bhatiaprateek111@gmail.com>
2023-12-26 23:37:47 +0530

    veux: crDroid: init some flags
    Signed-off-by: Prateek Bhatia <bhatiaprateek111@gmail.com>

--- lineage_veux.mk
+++ lineage_veux.mk
@@ -9,12 +9,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
 
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
