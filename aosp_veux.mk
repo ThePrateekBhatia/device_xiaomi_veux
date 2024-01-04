8e4d1814e38208a724dac9556f1073c1bdfb4dde
Prateek Bhatia <bhatiaprateek111@gmail.com>
2024-01-04 09:56:29 +0530

    veux: elixir: Ready for Project Elixir
    Signed-off-by: Prateek Bhatia <bhatiaprateek111@gmail.com>

--- aosp_veux.mk
+++ aosp_veux.mk
@@ -1,25 +1,37 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024 The Project Elixir
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-TARGET_FACE_UNLOCK_SUPPORTED := true
+# Common flags
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ACORE := false
+
+# Add useful flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_CALL_RECORDING := true
 TARGET_SUPPORTS_QUICK_TAP := true
+FORCE_AOSP_DIALER := true
+
+# Elixir flags
 IS_OFFICIAL := true
+ELIXIR_BUILD_TYPE := OFFICIAL
+ELIXIR_MAINTAINER := ꜰʀᴇᴀᴋʏ-ᴘʀᴀᴛᴇᴇᴋ
+BUILD_USERNAME := Freaky
+BUILD_HOSTNAME := Elixir
 
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-# Inherit some common PixelOS
+# Inherit some common Project Elixir Stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_veux
