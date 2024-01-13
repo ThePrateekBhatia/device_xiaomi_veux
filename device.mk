a860847713051fefa626096ada53ad845cc78617
K R I S H N A <krishnakripa34567@gmail.com>
2024-01-13 21:35:14 +0530

    veux: call the local overlay packages from tree

--- device.mk
+++ device.mk
@@ -352,6 +352,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Perf
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2 \
