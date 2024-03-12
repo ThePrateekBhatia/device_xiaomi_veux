1223101fc51af2a04109c5bbd05a16d1ec71754f
Prateek Bhatia <bhatiaprateek111@gmail.com>
2024-03-12 19:57:13 +0000

    commit

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,6 @@ git clone https://gitlab.com/JaswalAshish/vendor_xiaomi_veux vendor/xiaomi/veux
 
 echo 'Cloning Kernel tree [2/2]'
 # Kernel Tree
-git clone --depth=1 https://github.com/dereference23/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
+git clone https://github.com/dereference23/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
 
 echo 'Completed, proceeding to lunch'
