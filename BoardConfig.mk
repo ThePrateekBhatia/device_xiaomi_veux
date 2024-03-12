5f6e1a91b895a1c835d1024f927d1c38318e83cd
Prateek Bhatia <bhatiaprateek111@gmail.com>
2024-03-12 20:49:57 +0000

    commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6375
 TARGET_KERNEL_CONFIG := veux_defconfig
 TARGET_KERNEL_NO_GCC := true
 
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x04C8C000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=4e00000.dwc3 swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1 firmware_class.path=/vendor/firmware
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x04C8C000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=4e00000.dwc3 swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1 firmware_class.path=/vendor/firmware swinfo.fingerprint=$(EVO_VERSION) mtdoops.fingerprint=$(EVO_VERSION) allow_file_spec_access irqaffinity=0-3 pelt=8
 
 # Prebuilt DTBO Image
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
